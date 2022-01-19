2e366fefd05ccaed79a412d49c0f962dc3b92960
AntonioSkopin <askopin@outlook.com>
2022-01-19 19:40:23 +0100

    #1 Fixed horizontal scrolling

--- src/Components/Projects/ProjectItem.jsx
+++ src/Components/Projects/ProjectItem.jsx
@@ -3,7 +3,7 @@ import "./Styles/project.css";
 const ProjectItem = props => {
     return (
         <div className="menu__item py-4">
-            <a href={props.link} className="menu__item-link font-bold text-7xl md:text-9xl">{props.projectTitle}</a>
+            <a href={props.link} className="menu__item-link font-bold text-5xl md:text-9xl">{props.projectTitle}</a>
             <img className="menu__item-img" src={props.projectIMG} alt="Project image"/>
             <div className="marquee font-bold text-7xl md:text-9xl">
                 <div className="marquee__inner" aria-hidden="true">
--- src/Sections/AboutMe.jsx
+++ src/Sections/AboutMe.jsx
@@ -4,34 +4,11 @@ import OrangeButton from "../Components/Buttons/OrangeButton";
 const AboutMe = () => {
     return (
         <div className="container mx-auto px-4 py-24">
-            {/* <div>
-                <h1 className="font-light text-3xl xl:w-1/2 leading-relaxed">
-                    A fullstack webdeveloper driven by ideas and
-                    enthusiasm. My goal is to build user friendly websites
-                    and help businesses grow.
-                </h1>
-            </div>
-            <div className="h-auto py-24 flex justify-between">
-                <h1 className="w-4/5 text-xl sm:text-2xl font-semibold">2019 - Present</h1>
-                <p className="font-light text-xl leading-loose">
-                    My design style is simplistic yet effective and I design with a purpose! 
-                    I’m well-versed in producing mobile/desktop applications and websites which satisfy 
-                    clients and keep users happy.
-
-                    I love turning my ideas into reality.
-                </p>
-            </div>
-            <div className="text-center">
-                <OrangeButton 
-                    text="Download CV"
-                    textColor="button-text-black"
-                    width="15rem" />
-            </div> */}
             <h1 className="text-2xl md:text-5xl font-light leading-loose md:leading-relaxed">
                 A fullstack webdeveloper driven by ideas and
                 enthusiasm. My goal is to build user friendly websites
                 and help businesses grow. My design style is simplistic yet effective and I design with a purpose! 
-                I’m well-versed in producing mobile/desktop applications and websites which satisfy 
+                I'm well-versed in producing mobile/desktop applications and websites which satisfy 
                 clients and keep users happy.
             </h1>
             <div className="text-center py-8">
